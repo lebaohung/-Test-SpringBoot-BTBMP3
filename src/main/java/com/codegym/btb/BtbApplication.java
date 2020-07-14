3c15955fc618ea1aa95e66b3f60b4d512f71b9fd
lebaohung.1996 <lebaohung.1996@gmail.com>
2020-07-14 15:39:05 +0700

    init SpringBoot

---
+++ src/main/java/com/codegym/btb/BtbApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.btb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtbApplication.class, args);
+    }
+
+}
